6ac736f31d3d09ed0c1fd438f9d52493e2e248b4
Jacob Walser <jwalser90@gmail.com>
2023-12-18 20:12:17 -0500

    flash: add new sources for Ping360 flashing

---
+++ src/flash/ping360flasher.cpp
@@ -0,0 +1,23 @@
+#include "ping360flasher.h"
+
+Ping360Flasher::Ping360Flasher(QObject* parent)
+    : Flasher(parent, {115200})
+{
+    connect(
+        &_worker, &Ping360FlashWorker::flashProgressChanged, this,
+        [this](float flashProgressPct) { emit flashProgress(flashProgressPct); }, Qt::QueuedConnection);
+    connect(
+        &_worker, &Ping360FlashWorker::stateChanged, this, [this](Flasher::States newState) { setState(newState); },
+        Qt::QueuedConnection);
+    connect(
+        &_worker, &Ping360FlashWorker::messageChanged, this, [this](QString message) { setMessage(message); },
+        Qt::QueuedConnection);
+}
+
+void Ping360Flasher::flash()
+{
+    _worker.setFirmwarePath(_firmwareFilePath);
+    _worker.setLink(_link);
+    _worker.setVerify(_verify);
+    _worker.start();
+}
